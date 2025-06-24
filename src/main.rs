773632da329e4e9b1926d65a8d8993741117ae11
Gyeongo Eo <>
2025-06-25 02:28:53 +0900

    test: multi dimension case

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,50 @@ fn main() {
     tensor2d.set(2, 1, 8f32);
     tensor2d.set(2, 2, 9f32);
 
-
     println!("{:?}", tensor2d.get_row(0));
     println!("{:?}", tensor2d.get_row(1));
     println!("{:?}", tensor2d.get_row(2));
 
-
-
-    let column= tensor2d.get_column(2);
+    let column = tensor2d.get_column(2);
     println!("{:?}", column);
+
+    let mut tensor3d = Tensor::new(&[2, 2, 2, 2]);
+    tensor3d.set(&[0, 0, 0, 0], 1f32);
+    tensor3d.set(&[0, 0, 0, 1], 3f32);
+    tensor3d.set(&[0, 0, 1, 0], 2f32);
+    tensor3d.set(&[0, 0, 1, 1], 4f32);
+    tensor3d.set(&[0, 1, 0, 0], 5f32);
+    tensor3d.set(&[0, 1, 0, 1], 6f32);
+    tensor3d.set(&[0, 1, 1, 0], 7f32);
+    tensor3d.set(&[0, 1, 1, 1], 8f32);
+    tensor3d.set(&[1, 0, 0, 0], 9f32);
+    tensor3d.set(&[1, 0, 0, 1], 10f32);
+    tensor3d.set(&[1, 0, 1, 0], 11f32);
+    tensor3d.set(&[1, 0, 1, 1], 12f32);
+    tensor3d.set(&[1, 1, 0, 0], 13f32);
+    tensor3d.set(&[1, 1, 0, 1], 14f32);
+    tensor3d.set(&[1, 1, 1, 0], 15f32);
+    tensor3d.set(&[1, 1, 1, 1], 16f32);
+    
+    for a in 0..2 {
+        for b in 0..2 {
+            for c in 0..2 {
+                for d in 0..2 {
+                    println!("{}", tensor3d.get(&[a, b, c, d]));
+                }
+            }
+        }
+    }
 }
 
 struct Tensor2D {
-    row: u32,
-    column: u32,
+    row: usize,
+    column: usize,
     core: Tensor,
 }
 
 impl Tensor2D {
-    pub fn new(row: u32, column: u32) -> Self {
+    pub fn new(row: usize, column: usize) -> Self {
         Tensor2D {
             row,
             column,
@@ -46,15 +71,15 @@ impl Tensor2D {
         }
     }
 
-    pub fn get(&self, row: u32, column: u32) -> f32 {
+    pub fn get(&self, row: usize, column: usize) -> f32 {
         self.core.get(&[row, column])
     }
 
-    pub fn set(&mut self, row: u32, column: u32, value: f32) {
+    pub fn set(&mut self, row: usize, column: usize, value: f32) {
         self.core.set(&[row, column], value);
     }
 
-    pub fn get_row(&self, row: u32) -> Box<[f32]> {
+    pub fn get_row(&self, row: usize) -> Box<[f32]> {
         let mut row_buffer = vec![0f32; self.column as usize];
         for c in 0..self.column {
             row_buffer[c as usize] = self.get(row, c);
@@ -62,7 +87,7 @@ impl Tensor2D {
         row_buffer.into_boxed_slice()
     }
 
-    pub fn get_column(&self, column: u32) -> Box<[f32]> {
+    pub fn get_column(&self, column: usize) -> Box<[f32]> {
         let mut column_buffer = vec![0f32; self.row as usize];
         for r in 0..self.row {
             column_buffer[r as usize] = self.get(r, column);
@@ -72,14 +97,15 @@ impl Tensor2D {
 }
 
 struct Tensor {
-    shape: Box<[u32]>,
+    pos_base: usize,
+    shape: Box<[usize]>,
     array: Box<[f32]>,
 }
 
 impl Tensor {
-    pub fn new(shape: &[u32]) -> Self {
+    pub fn new(shape: &[usize]) -> Self {
         let array: Box<[f32]>;
-        let mut size = 1u32;
+        let mut size = 1usize;
 
         for x in shape {
             size *= x;
@@ -88,40 +114,47 @@ impl Tensor {
         if size == 0 {
             array = vec![].into_boxed_slice();
         } else {
-            array = vec![0f32; size as usize].into_boxed_slice();
+            array = vec![0f32; size].into_boxed_slice();
+        }
+
+        let mut pos_base = 1;
+        for i in 0..shape.len() - 1 {
+            pos_base *= shape[i];
         }
 
         Tensor {
+            pos_base,
             shape: shape.into(),
             array,
         }
     }
 
-    pub fn set(&mut self, pos: &[u32], value: f32) {
+    pub fn set(&mut self, pos: &[usize], value: f32) {
         self.assert_match_shape(pos);
         let idx = self.flatten_pos(pos);
         self.array[idx] = value;
     }
 
-    pub fn get(&self, pos: &[u32]) -> f32 {
+    pub fn get(&self, pos: &[usize]) -> f32 {
         self.assert_match_shape(pos);
         let idx = self.flatten_pos(pos);
         self.array[idx]
     }
 
-    fn flatten_pos(&self, pos: &[u32]) -> usize {
-        let mut base = 0;
+    fn flatten_pos(&self, pos: &[usize]) -> usize {
+        let mut result = 0;
 
+        let mut pos_base = self.pos_base;
         for i in 0..pos.len() - 1 {
-            let p = pos[i];
-            base += p * self.shape[i];
+            result += pos[i] * pos_base;
+            pos_base /= self.shape[i];
         }
 
-        base += pos[pos.len() - 1];
-        base as usize
+        result += pos[pos.len() - 1];
+        result as usize
     }
 
-    fn assert_match_shape(&self, shape: &[u32]) {
+    fn assert_match_shape(&self, shape: &[usize]) {
         if self.shape.len() != shape.len() {
             panic!(
                 "Shape mismatch: expected {}, got {}",
